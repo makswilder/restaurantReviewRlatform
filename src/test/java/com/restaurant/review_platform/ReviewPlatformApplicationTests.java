31bc520f4de2df28e9fa6b18851bd1e0adeeb8c2
makswilder <maks.shherbakov04@gmail.com>
2025-07-14 19:53:17 +0200

    Restaurant Review Platform

---
+++ src/test/java/com/restaurant/review_platform/ReviewPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.restaurant.review_platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReviewPlatformApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
