3b6cdbe286832861e1e15bb7e025173bb9a7e4a8
makswilder <maks.shherbakov04@gmail.com>
2025-08-20 15:12:46 +0200

    chore: added mapStruct dependency and configured maven compile plugin

--- src/main/java/com/restaurant/review_platform/ReviewPlatformApplication.java
+++ src/main/java/com/restaurant/review_platform/ReviewPlatformApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ReviewPlatformApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ReviewPlatformApplication.class, args);
 	}
-
 }
