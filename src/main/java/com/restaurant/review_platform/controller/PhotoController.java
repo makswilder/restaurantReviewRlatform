009cfca560d115a50ee385465fd395fefee8c858
makswilder <maks.shherbakov04@gmail.com>
2025-09-22 15:42:46 +0200

    feat(photo): added GET /api/photos endpoint and allowed public access

--- src/main/java/com/restaurant/review_platform/controller/PhotoController.java
+++ src/main/java/com/restaurant/review_platform/controller/PhotoController.java
@@ -5,6 +5,13 @@ import com.restaurant.review_platform.domain.entity.Photo;
 import com.restaurant.review_platform.mapper.PhotoMapper;
 import com.restaurant.review_platform.service.PhotoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.MediaTypeFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,5 +30,17 @@ public class PhotoController {
     public PhotoDto uploadPhoto(@RequestParam("file")MultipartFile file) {
         Photo savedPhoto = photoService.uploadPhoto(file);
         return photoMapper.toDto(savedPhoto);
+    }
+
+    @GetMapping(path = "/{id:.+}")
+    public ResponseEntity<Resource> getPhoto(@PathVariable("id") String id) {
+        return photoService.getPhotoAsResource(id).map(photo ->
+            ResponseEntity.ok()
+                .contentType(MediaTypeFactory.getMediaType(photo)
+                    .orElse(MediaType.APPLICATION_OCTET_STREAM)
+                )
+                .header(HttpHeaders.CONTENT_DISPOSITION, "inline")
+                .body(photo)
+        ).orElse(ResponseEntity.notFound().build());
     }
 }
